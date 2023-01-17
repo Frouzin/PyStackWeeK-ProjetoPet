07f76f93f7d40ade40a359b14c3fc3d8673c18c5
Frouzin <matheusereis44@gmail.com>
2023-01-17 00:48:13 -0300

    alterações no banco de dados e melhoramento de sistema

--- divulgar/admin.py
+++ divulgar/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Raca
+from .models import Tag,Pet
+
+admin.site.register(Raca)
+admin.site.register(Tag)
+admin.site.register(Pet)
 
 # Register your models here.
