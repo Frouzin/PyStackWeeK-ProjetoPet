587d2e3b500b697d615c4ccff3127ab083fd46a4
Frouzin <matheusereis44@gmail.com>
2023-01-25 13:51:44 -0300

    nova pagina e salvar novo pet no banco de dados!

--- divulgar/views.py
+++ divulgar/views.py
@@ -1,8 +1,37 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-
+from .models import Tag,Raca,Pet
 
 # Create your views here.
+@login_required
 def novo_pet(request):
-    return HttpResponse('oi')
+    if request.method == "GET":
+        tags = Tag.objects.all()
+        raca = Raca.objects.all()
+        return render(request,'novo_pet.html', {'tags':tags,'racas':raca})
+    elif request.method == "POST":
+        foto = request.FILES.get('foto')
+        nome = request.POST.get('nome')
+        descricao = request.POST.get('descricao')
+        estado = request.POST.get('estado')
+        cidade = request.POST.get('cidade')
+        telefone = request.POST.get('telefone')
+        tags = request.POST.getlist('tags')
+        raca = request.POST.get('raca')
+
+
+        # VALIDAR DADOS
+
+        pet = Pet(
+            usuario=request.user,
+            foto=foto,
+            nome=nome,
+            descricao=descricao,
+            estado=estado,
+            cidade=cidade,
+            telefone=telefone,
+            raca_id=raca,
+        )
+
+        pet.save()
