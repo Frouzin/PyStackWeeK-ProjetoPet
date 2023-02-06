75f0de0c5662f460325045aa112ceda0b82abd66
Frouzin <matheusereis44@gmail.com>
2023-02-06 14:21:52 -0300

    criação de dashboard e consumir uma api

--- divulgar/views.py
+++ divulgar/views.py
@@ -6,6 +6,7 @@ from django.contrib.messages import constants
 from django.shortcuts import redirect
 from django.contrib import messages
 from adotar.models import PedidoAdocao
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 @login_required
@@ -77,3 +78,22 @@ def ver_pedido_adocao(request):
     if request.method == "GET":
         pedidos = PedidoAdocao.objects.filter(usuario=request.user).filter(status="AG")
         return render(request, 'ver_pedido_adocao.html', {'pedidos': pedidos})
+
+def dashboard(request):
+    if request.method == "GET":
+        return render(request, 'dashboard.html')
+
+@csrf_exempt
+def api_adocoes_por_raca(request):
+    racas = Raca.objects.all()
+
+    qtd_adocoes = []
+    for raca in racas:
+        adocoes = PedidoAdocao.objects.filter(pet__raca=raca).count()
+        qtd_adocoes.append(adocoes)
+
+    racas = [raca.raca for raca in racas]
+    data = {'qtd_adocoes': qtd_adocoes,
+            'labels': racas}
+
+    return JsonResponse(data)
