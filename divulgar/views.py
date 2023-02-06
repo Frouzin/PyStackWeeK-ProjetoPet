c49f2935176156f6a80522a5a15a5f8c00617203
Frouzin <matheusereis44@gmail.com>
2023-02-06 13:50:51 -0300

    pedido de adoção

--- divulgar/views.py
+++ divulgar/views.py
@@ -5,6 +5,8 @@ from .models import Tag,Raca,Pet
 from django.contrib.messages import constants
 from django.shortcuts import redirect
 from django.contrib import messages
+from adotar.models import PedidoAdocao
+
 # Create your views here.
 @login_required
 def novo_pet(request):
@@ -70,3 +72,8 @@ def ver_pet(request, id):
     if request.method == "GET":
         pet = Pet.objects.get(id = id)
         return render(request, 'ver_pet.html', {'pet': pet})
+
+def ver_pedido_adocao(request):
+    if request.method == "GET":
+        pedidos = PedidoAdocao.objects.filter(usuario=request.user).filter(status="AG")
+        return render(request, 'ver_pedido_adocao.html', {'pedidos': pedidos})
