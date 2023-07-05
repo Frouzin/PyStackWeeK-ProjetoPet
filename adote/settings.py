0108d22dbc72c51d3a6bb4993980c3381f66dda4
Frouzin <matheusereis44@gmail.com>
2023-07-05 11:06:03 -0300

    nova atualização para deploy

--- adote/settings.py
+++ adote/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-+@c@(^lj*tml6+ymgeke24z821_uj$$qnlbffr0ykdg0q-^!sl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
